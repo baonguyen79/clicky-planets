e8a0b75b3ac3d7dca128dfb84f0c1d1a90486fcc
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-02-21 18:59:33 -0600

    add dom

---
+++ main.js
@@ -0,0 +1,6 @@
+var myButton = document.getElementById("showPlanets");
+myButton.addEventListener("click", myAdd);
+
+ function myAdd (){
+	document.getElementById("planetHolder").innerHTML = "A new Planet";
+} 
