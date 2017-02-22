9576821d3818a3bef6934c04f29ed4daeac60a18
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-02-21 20:04:22 -0600

    style

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 var myButton = document.getElementById("showPlanets");
-myButton.addEventListener("click", myAdd);
+var outDom = document.getElementById("planetHolder");
+var planets = ["mercury", "venus", "earth", "mars", "jupiter", "uranus", "neptune"];
 
- function myAdd (){
-	document.getElementById("planetHolder").innerHTML = "A new Planet";
-} 
+var Planets = [
+{
+	name:"Mercury",
+	url:""
+},
+{
+	name:"Venus",
+	url:""
+},
+{
+	name:"Earth",
+	url:""
+},
+{
+	name:"Mars",
+	url:""
+},
+{
+	name:"Jupiter",
+	url:""
+},
+{
+	name:"Uranus",
+	url:""
+},
+{
+	name:"Neptune",
+	url:""
+}
+];
+
+
+function writeToDom (){
+	outDom.innerHTML = "";
+	for (var i=0; i<planets.length; i++)
+ 		{
+ 			var newPlanet="";
+ 			newPlanet += `<div class="planetBox", id="planetBox-${i}">`
+ 			newPlanet += `<div class="planetName"> ${Planets[i].name} </div>`; 
+			newPlanet += `</div>`;
+ 			outDom.innerHTML += newPlanet; 
+		}
+	}
+
+myButton.addEventListener("click", writeToDom);
+
+
